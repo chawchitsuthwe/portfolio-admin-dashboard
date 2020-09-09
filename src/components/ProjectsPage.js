c24ef7e59e2d2c9c1be027ac35754380e4b1a32c
Chaw Chit Su Thwe <61979480+chawchitsuthwe@users.noreply.github.com>
2020-09-09 20:34:33 +0630

    Finished Side Nav

---
+++ src/components/ProjectsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+
+const ProjectsPage = () => {
+	return (
+		<div>
+			<Grid container>
+				<Grid item xs="12">
+
+				</Grid>
+			</Grid>
+		</div>
+	)
+}
+
+export default ProjectsPage;
